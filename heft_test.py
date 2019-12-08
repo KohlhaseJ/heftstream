63b2431acf5eca50f7722cafc111bcf1d769e47e
Noah WÃöhler <noah.woehler@gmail.com>
2019-12-09 00:41:27 +0100

    Preprocessing: Show target columns before training

--- heft_test.py
+++ heft_test.py
@@ -7,12 +7,14 @@ from skmultiflow.meta import AccuracyWeightedEnsemble
 from src.heft.meta.heterogeneous_ensemble_for_featuredrifts import HeterogenousEnsembleForFeatureDrifts
 
 # 1. Create a stream
-#stream = FileStream("data/unpacked/kdd.csv")
+stream = FileStream("data/unpacked/kdd_1_hot.csv")
+
 #stream.prepare_for_use()
-stream = SEAGenerator(noise_percentage=0.1)
+#stream = SEAGenerator(noise_percentage=0.1)
 #stream = HyperplaneGenerator(noise_percentage=0.05, mag_change=0.001, n_drift_features=10) # TODO: n_drift_features = changing 10 attributes at speed ...?
 #stream = LEDGenerator(noise_percentage=0.1, has_noise=True)
 stream.prepare_for_use()
+print(stream.target_names)
 
 # 2. Instantiate the Ensemble
 ensemble = HeterogenousEnsembleForFeatureDrifts(verbose=1, window_size=1000, n_kept_estimators=10)
