c49ec8ddafe5b6a529b4beef15938bb92ff18131
Noah WÃöhler <noah.woehler@gmail.com>
2019-12-19 22:05:56 +0100

    Preprocessing kdd: converting nominal features -> ordinal
    Does not solve the problem that FCBF selects 0 features
    in every iteration.

--- preprocess_data.py
+++ preprocess_data.py
@@ -33,7 +33,7 @@ def nominal_features_to_binary(df, cols):
 
 def nominal_features_to_bdc(df, cols):
     bdc = ce.BackwardDifferenceEncoder(cols=cols)
-    df_bdc = bin.fit_transform(df)
+    df_bdc = bdc.fit_transform(df)
     print(df_bdc.info())
     return df_bdc
 
@@ -46,10 +46,12 @@ if __name__ == '__main__':
     class_col = 41
 
     df_numerical_class = nominal_feature_to_numerical(df, class_col)
-    df_binary = nominal_features_to_binary(df_numerical_class, nominal_feature_cols)
 
-    print(df_binary.head())
+    for col in nominal_feature_cols:
+        df_numerical_class = nominal_feature_to_numerical(df_numerical_class, col)
 
-    df_binary.to_csv('data/unpacked/kdd_binary.csv', index=False)
+    print(df_numerical_class.head())
+
+    df_numerical_class.to_csv('data/unpacked/kdd_ordinal.csv', index=False)
 
 
