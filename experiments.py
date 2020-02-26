62465c0cb3ee0fbe7b8d0d224db064fd2ce0fe34
sebastian <sebastiandoehler@hotmail.de>
2020-02-26 11:54:03 +0100

    experiment setup

--- experiments.py
+++ experiments.py
@@ -1,13 +1,14 @@
-from skmultiflow.data import FileStream, DataStream
+from skmultiflow.data import FileStream
 from skmultiflow.bayes import NaiveBayes
 from skmultiflow.trees.hoeffding_adaptive_tree import HAT
 from skmultiflow.trees.hoeffding_anytime_tree import HATT
 from skmultiflow.meta import AccuracyWeightedEnsemble
 from src.heft.meta.heterogeneous_ensemble_for_featuredrifts import HeterogeneousEnsembleForFeatureDrifts
 from src.heft.feature_selection.fcbf import FCBF
-from src.heft.feature_selection.cife import CIFE
 from skmultiflow.evaluation import EvaluatePrequential
-from src.heft.classifier.CalibratedPerceptron import CalibratedPerceptron
+from itertools import combinations
+from functools import partial
+from sklearn.neural_network import MLPClassifier
 import sys
 from io import StringIO
 
@@ -15,15 +16,15 @@ from io import StringIO
 open('results.txt', 'w').close()
 
 # 1. Create a list of filepaths for all datasets
-files = ['data/LED.csv', 'data/SEA.csv']
+files = ['data/KDD_PRE.csv', 'data/weather.csv', 'data/LED.csv', 'data/SEA.csv', 'data/HYP.csv']
 MAX_SAMPLES = 1000
-# [NaiveBayes(), CalibratedPerceptron()], [HATT(), CalibratedPerceptron()], [HAT(), CalibratedPerceptron()]
-HEFT_BASE_ESTIMATORS_LIST = [[NaiveBayes(), HATT()], [NaiveBayes(), HAT()], [HATT(), HAT()]]
-# CIFE
-FEATURE_SELECTOR_LIST = [FCBF]
-# stream = FileStream("data/HYP.csv")
-# stream = FileStream("data/KDD_PRE.csv")
-# stream = FileStream("data/MNIST.csv")
+BASE_ESTIMATORS = [NaiveBayes, HATT, HAT, partial(MLPClassifier, learning_rate_init=1e-2, hidden_layer_sizes=1, max_iter=2000)]
+HEFT_BASE_ESTIMATORS_LIST = []
+for i in range(1, len(BASE_ESTIMATORS) + 1):
+    HEFT_BASE_ESTIMATORS_LIST.extend(list(combinations(BASE_ESTIMATORS, i)))
+print(len(HEFT_BASE_ESTIMATORS_LIST))
+print(HEFT_BASE_ESTIMATORS_LIST)
+FEATURE_SELECTOR_LIST = [FCBF, ]
 
 for file in files:
     for FEATURE_SELECTOR in FEATURE_SELECTOR_LIST:
@@ -37,7 +38,7 @@ for file in files:
             n_kept_estimators = 10
             ensembles = {
                 'heft': HeterogeneousEnsembleForFeatureDrifts(window_size=window_size, n_kept_estimators=n_kept_estimators,
-                                                              min_features=5, base_estimators=HEFT_BASE_ESTIMATORS,
+                                                              min_features=5, base_estimators=[c() for c in HEFT_BASE_ESTIMATORS],
                                                               feature_selector=FEATURE_SELECTOR,
                                                               random_ensemble=0),
                 'awe': AccuracyWeightedEnsemble(window_size=window_size, n_kept_estimators=n_kept_estimators,
@@ -60,7 +61,7 @@ for file in files:
                 evaluator.evaluation_summary()
                 s.seek(0)
                 f.write('----------\n')
-                f.write(file + '\n')
+                f.write(file + str(HEFT_BASE_ESTIMATORS) + '\n')
                 f.write(str(s.read()))
                 f.write(str(ensembles['heft'].print_statistics()))
                 f.write('----------\n')
